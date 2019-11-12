13fb202689eadbefd8e9c8f5ac7c1a34b814df26
NullArray <NullArray@users.noreply.github.com>
2019-11-12 20:11:06 +0100

    Add files via upload

---
+++ SysBin/WindowsLM/BlockWindows.bat
@@ -0,0 +1,111 @@
+@ECHO OFF
+REM --- Start by right clicking this file and "run as admin"
+SETLOCAL
+
+REM --- uninstall updates
+echo uninstalling updates ...
+echo Delete KB971033 (license validation)
+start "title" /b /wait wusa.exe /kb:971033 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB2902907 (Microsoft Security Essentials)
+start "title" /b /wait wusa.exe /kb:2902907 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB2952664 (Get Windows 10 assistant)
+start "title" /b /wait wusa.exe /kb:2952664 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB2976978 (Compatibility update for Windows 8.1 and Windows 8)
+start "title" /b /wait wusa.exe /kb:2976978 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB2977759 (Compatibility update for Windows 7 RTM)
+start "title" /b /wait wusa.exe /kb:2977759 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB2990214 (Get Windows 10 for Win7)
+start "title" /b /wait wusa.exe /kb:2990214 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB3012973 (Upgrade to Windows 10 Pro)
+start "title" /b /wait wusa.exe /kb:3012973 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB3021917 (Update to Windows 7 SP1 for performance improvements)
+start "title" /b /wait wusa.exe /kb:3021917 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB3022345 (telemetry)
+start "title" /b /wait wusa.exe /kb:3022345 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB3035583 (GWX Update installs Get Windows 10 app in Windows 8.1 and Windows 7 SP1)
+start "title" /b /wait wusa.exe /kb:3035583 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB3044374 (Get Windows 10 for Win8.1)
+start "title" /b /wait wusa.exe /kb:3044374 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB3068708 (telemetry)
+start "title" /b /wait wusa.exe /kb:3068708 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB3075249 (telemetry for Win7/8.1)
+start "title" /b /wait wusa.exe /kb:3075249 /uninstall /quiet /norestart
+echo  - next
+echo Delete KB3080149 (Telemetry)
+start "title" /b /wait wusa.exe /kb:3080149 /uninstall /quiet /norestart
+echo  - done.
+timeout 5
+
+REM --- Hide updates
+echo Hiding updates...
+echo (...this will take a while)
+start "title" /b /wait cscript.exe "%~dp0HideWindowsUpdates.vbs" 971033 2902907 2952664 2976978 2977759 2990214 3012973 3021917 3022345 3035583 3044374 3068708 3075249 3080149 
+echo  - done.
+
+REM --- Block Routes
+echo Blocking Routes...
+route -p add 23.218.212.69 MASK 255.255.255.255 0.0.0.0
+route -p add 65.55.108.23 MASK 255.255.255.255 0.0.0.0
+route -p add 65.39.117.230 MASK 255.255.255.255 0.0.0.0
+route -p add 134.170.30.202 MASK 255.255.255.255 0.0.0.0
+route -p add 137.116.81.24 MASK 255.255.255.255 0.0.0.0
+route -p add 204.79.197.200 MASK 255.255.255.255 0.0.0.0
+route -p add 23.218.212.69 MASK 255.255.255.255 0.0.0.0
+
+REM --- Disable tasks
+echo Disabling tasks...
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\AitAgent" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\ProgramDataUpdater" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Autochk\Proxy" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Maintenance\WinSAT" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ActivateWindowsSearch" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ConfigureInternetTimeService" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\DispatchRecoveryTasks" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ehDRMInit" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\InstallPlayReady" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\mcupdate" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\MediaCenterRecoveryTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ObjectStoreRecoveryTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\OCURActivate" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\OCURDiscovery" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PBDADiscovery" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PBDADiscoveryW1" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PBDADiscoveryW2" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PvrRecoveryTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\PvrScheduleTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\RegisterSearch" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\ReindexSearchRoot" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\SqlLiteRecoveryTask" /DISABLE
+schtasks /Change /TN "\Microsoft\Windows\Media Center\UpdateRecordPath" /DISABLE
+echo - done
+
+REM --- Kill services
+echo Killing Diagtrack-service (if it still exists)...
+sc stop Diagtrack
+sc delete Diagtrack
+echo - done
+
+echo Stop remoteregistry-service (if it still exists)...
+sc config remoteregistry start= disabled
+sc stop remoteregistry
+
+echo Done - Manually reboot for changes to take effect
+REM shutdown -r
+pause
---
+++ SysBin/WindowsLM/README.md
@@ -0,0 +1,16 @@
+# BlockWindows
+Stop Windows 7 through 10 Nagging and Spying updates, Tasks, IPs, and services. Works with Windows 7 through 10
+
+FILES
+-----
+
+1. BlockWindows.bat : (Right Click and "Run as Admin")
+2. hosts.bat : Works with Windows 7 and 8. Appends current hosts file. Run from your Downloads directory.
+   Doesn't work on Windows 10, copy hosts file to your router or firewall if using Windows 10
+3. hosts : DNS file of MS hosts to block
+4. for Firewall/hostlist : MS Hosts file to blocking for router or firewall use
+5. HideWindowsUpdates.vbs : Hides blocked updates, to reinstall click 'show hidden updates'
+6. DisableWiFiSense.reg : disable WiFi password sharing
+
+--
+https://blockwindows.wordpress.com/ created the original code
